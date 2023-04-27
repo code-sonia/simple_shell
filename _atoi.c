29a09950fef2c828503705be19c6560dba3a33df
Sonia <delvedeepdream@gmail.com>
2023-04-26 17:14:29 -0700

    update

--- _atoi.c
+++ _atoi.c
@@ -8,7 +8,7 @@
  */
 int interactive(info_t *info)
 {
-        return (isatty(STDIN_FILENO) && info->readfd <= 2);
+       return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
 /**
@@ -19,10 +19,10 @@ int interactive(info_t *info)
  */
 int is_delim(char c, char *delim)
 {
-        while (*delim)
-                if (*delim++ == c)
-                        return (1);
-        return (0);
+       while (*delim)
+               if (*delim++ == c)
+                       return (1);
+       return (0);
 }
 
 /**
@@ -33,10 +33,10 @@ int is_delim(char c, char *delim)
 
 int _isalpha(int c)
 {
-        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-                return (1);
-        else
-                return (0);
+       if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+               return (1);
+       else
+               return (0);
 }
 
 /**
@@ -47,10 +47,10 @@ int _isalpha(int c)
 
 int _atoi(char *s)
 {
-        int i, sign = 1, flag = 0, output;
-        unsigned int result = 0;
+       int i, sign = 1, flag = 0, output;
+       unsigned int result = 0;
 
-        for (i = 0; s[i] != '\0' && flag != 2; i++)
+       for (i = 0; s[i] != '\0' && flag != 2; i++)
         {
                 if (s[i] == '-')
                         sign *= -1;
